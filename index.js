bc3345efc64ef0ab3790e520dcdbd70c02cce409
Ali Hassan <124985619+AliHassan521@users.noreply.github.com>
2023-06-13 23:35:40 +0500

    Add a repo Drum-Kit

---
+++ index.js
@@ -0,0 +1,48 @@
+let numOfButton = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numOfButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttontext = this.textContent;
+    makeSound(buttontext);
+   
+  });
+}
+
+document.addEventListener("keypress", function (event){
+    makeSound(event.key);
+})
+
+function makeSound (key){
+       switch (key) {
+         case "w":
+           const tom1 = new Audio("sounds/tom-1.mp3");
+           tom1.play();
+           break;
+         case "a":
+           const tom2 = new Audio("sounds/tom-2.mp3");
+           tom2.play();
+           break;
+         case "s":
+           const tom3 = new Audio("sounds/tom-3.mp3");
+           tom3.play();
+           break;
+         case "d":
+           const tom4 = new Audio("sounds/tom-4.mp3");
+           tom4.play();
+           break;
+         case "j":
+           const snare = new Audio("sounds/snare.mp3");
+           snare.play();
+           break;
+         case "k":
+           const crash = new Audio("sounds/crash.mp3");
+           crash.play();
+           break;
+         case "l":
+           const kick = new Audio("sounds/kick-bass.mp3");
+           kick.play();
+           break;
+         default:
+           console.log(key);
+       }
+}
